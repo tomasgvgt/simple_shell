78c0999b5e21261816e96f3b7cd6d565fa17a28b
Tomas Gomez <tomasgvg.t@gmail.com>
2020-04-09 17:09:46 +0000

    assitant_functions

--- holberton.h
+++ holberton.h
@@ -10,5 +10,8 @@
 
 int **str_to_array(char *command_line);
 int exec_new_programm(char **command_list);
+int _strlen(char *s);
+char *_strncpy(char *dest, char *src, int n);
+char *_strdup(char *str);
 
 #endif
